bd30ceecbc1682d2494dbfd0500f55d715ce6ee2
alecbar <alecbarnard@live.com>
2020-03-28 19:56:33 -0700

    Updates

---
+++ precache-manifest.0df321e20d7cc29e803323b43206a73c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2655500d0c50168fd59af090c305ef3b",
+    "url": "/alecbar/React-ToDo/index.html"
+  },
+  {
+    "revision": "d9bfd61bed6342c5cae0",
+    "url": "/alecbar/React-ToDo/static/css/main.f5d166d4.chunk.css"
+  },
+  {
+    "revision": "673d77e1aa2cb6c437bb",
+    "url": "/alecbar/React-ToDo/static/js/2.debffe2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alecbar/React-ToDo/static/js/2.debffe2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9bfd61bed6342c5cae0",
+    "url": "/alecbar/React-ToDo/static/js/main.8e76803f.chunk.js"
+  },
+  {
+    "revision": "d916c52c6c34202576b7",
+    "url": "/alecbar/React-ToDo/static/js/runtime-main.919e5394.js"
+  }
+]);
